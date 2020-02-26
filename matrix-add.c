e26fe66c46647860f83398977e98208f99c37cbf
Noor Mohammedb B <nooru.mohad@gmail.com>
2020-02-26 16:13:59 +0530

    matrix simplified

--- matrix-add.c
+++ matrix-add.c
@@ -6,35 +6,23 @@ int main()
 	scanf("%d%d",&r,&c);
 	printf("\nEnter First Matrix Elements\n");
 	for(i=0;i<r;i++)
-	{
 		for(j=0;j<c;j++)
-		{
 			scanf("%d",&m1[i][j]);
-		}
-	}
 	for(i=0;i<r;i++)
 	{
 		printf("\n");
 		for(j=0;j<c;j++)
-		{
 			printf("%d\t",m1[i][j]);
-		}
 	}
 	printf("\nEnter Second Matrix Elements\n");
 	for(i=0;i<r;i++)
-	{
 		for(j=0;j<c;j++)
-		{
 			scanf("%d",&m2[i][j]);
-		}
-	}
 	for(i=0;i<r;i++)
 	{
 		printf("\n");
 		for(j=0;j<c;j++)
-		{
 			printf("%d\t",m2[i][j]);
-		}
 	}
 	printf("\n\nResult = \n");
 	for(i=0;i<r;i++)
