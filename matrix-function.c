ac2d2b715158095d645143b92ddacebd0b20e5c4
NOOR MOHAMMED B <nooru.mohad@gmail.com>
2020-03-04 17:09:37 +0530

    matrix code completed

--- matrix-function.c
+++ matrix-function.c
@@ -1,60 +1,60 @@
 #include<stdio.h>
-void display(int m1[10][10], int m2[10][10], int r, int c)
+void display(int m1[5][5], int m2[5][5], int r, int c)
 {
-	int re[10][10],i,j;
-	printf("\nIn Function \n");
-	printf("\n1\n");
-	for(i=0;i<r;i++)
-	{
-		printf("\n");
-		for(j=0;j<c;j++)
-			printf("%d\t",m1[i][j]);
-	}
-	printf("\n2\n");
-	for(i=0;i<r;i++)
-	{
-		printf("\n");
-		for(j=0;j<c;j++)
-			printf("%d\t",m2[i][j]);
-	}
-	printf("\n\nResult = \n");
-	for(i=0;i<r;i++)
-	{
-	printf("\n");
-		for(j=0;j<c;j++)
-		{
-			re[i][j]=0;
-			re[i][j]=m1[i][j]+m2[i][j];
-			printf("%d\t",re[i][j]);
-		}
-	}
-	printf("\n");	
+        int re[10][10],i,j;
+        printf("\nIn Function \n");
+        printf("\nFirst matrix\n------------------------");
+        for(i=0;i<r;i++)
+        {
+                printf("\n");
+                for(j=0;j<c;j++)
+                        printf("%d\t",m1[i][j]);
+        }
+        printf("\nSecond matrix\n------------------------");
+        for(i=0;i<r;i++)
+        {
+                printf("\n");
+                for(j=0;j<c;j++)
+                        printf("%d\t",m2[i][j]);
+        }
+        printf("\n\nResult = \n==============================");
+        for(i=0;i<r;i++)
+        {
+        printf("\n");
+                for(j=0;j<c;j++)
+                {
+                        re[i][j]=0;
+                        re[i][j]=m1[i][j]+m2[i][j];
+                        printf("%d\t",re[i][j]);
+                }
+        }
+        printf("\n");
 }
 
 int main()
 {
-	int m1[5][5],m2[5][5],r,c,i,j;
-	printf("\nEnter row and colomn :  ");
-	scanf("%d%d",&r,&c);
-	printf("\nEnter First Matrix Elements\n");
-	for(i=0;i<r;i++)
-		for(j=0;j<c;j++)
-			scanf("%d",&m1[i][j]);
-	for(i=0;i<r;i++)
-	{
-		printf("\n");
-		for(j=0;j<c;j++)
-			printf("%d\t",m1[i][j]);
-	}
-	printf("\nEnter Second Matrix Elements\n");
-	for(i=0;i<r;i++)
-		for(j=0;j<c;j++)
-			scanf("%d",&m2[i][j]);
-	for(i=0;i<r;i++)
-	{
-		printf("\n");
-		for(j=0;j<c;j++)
-			printf("%d\t",m2[i][j]);
-	}
-	display(m1,m2, r, c);
+        int m1[5][5],m2[5][5],r,c,i,j;
+        printf("\nEnter row and colomn :  ");
+        scanf("%d%d",&r,&c);
+        printf("\nEnter First Matrix Elements\n");
+        for(i=0;i<r;i++)
+                for(j=0;j<c;j++)
+                        scanf("%d",&m1[i][j]);
+        for(i=0;i<r;i++)
+        {
+                printf("\n");
+                for(j=0;j<c;j++)
+                        printf("%d\t",m1[i][j]);
+        }
+        printf("\nEnter Second Matrix Elements\n");
+        for(i=0;i<r;i++)
+                for(j=0;j<c;j++)
+                        scanf("%d",&m2[i][j]);
+        for(i=0;i<r;i++)
+        {
+                printf("\n");
+                for(j=0;j<c;j++)
+                        printf("%d\t",m2[i][j]);
+        }
+        display(m1,m2, r, c);
 }
